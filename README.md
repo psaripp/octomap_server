2658fdd3e12b463fef84e9f0c6ed288552ad4978
Peter Sari <sari@photoneo.com>
2020-04-20 11:05:26 +0200

    Example octree publisher

---
+++ README.md
@@ -0,0 +1,20 @@
+# Octomap Server 
+
+Modified version of [`map_server`](https://github.com/ros-planning/navigation/tree/kinetic-devel/map_server) example from ROS Moveit. 
+
+## Prerequisites
+
+- [octomap](https://github.com/OctoMap/octomap.git), and [octomap_msgs](https://github.com/OctoMap/octomap_msgs.git)
+- Optional: [ROS Websocket bridge](http://wiki.ros.org/rosbridge_suite/Tutorials/RunningRosbridge)
+
+## Usage
+
+After `catkin_make` and initialize workspace:
+
+```
+rosrun octomap_server map_server [-l] [-b | -f] map.(bt|ot)
+    -f forces to send a full map
+    -b forces to send a binary map
+    -l latch published message
+```
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,263 +1,141 @@
-/*
- * Copyright (c) 2008, Willow Garage, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the Willow Garage, Inc. nor the names of its
- *       contributors may be used to endorse or promote products derived from
- *       this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
-
-/* Author: Brian Gerkey */
-
-#define USAGE "\nUSAGE: map_server <map.yaml>\n" \
-              "  map.yaml: map description file\n" \
-              "DEPRECATED USAGE: map_server <map> <resolution>\n" \
-              "  map: image file to load\n"\
-              "  resolution: map resolution [meters/pixel]"
-
-#include <stdio.h>
-#include <stdlib.h>
 #include <fstream>
-#include <boost/filesystem.hpp>
 
-#include "ros/ros.h"
 #include "ros/console.h"
-#include "map_server/image_loader.h"
-#include "nav_msgs/MapMetaData.h"
-#include "yaml-cpp/yaml.h"
+#include "ros/ros.h"
 
-#ifdef HAVE_YAMLCPP_GT_0_5_0
-// The >> operator disappeared in yaml-cpp 0.5, so this function is
-// added to provide support for code written under the yaml-cpp 0.3 API.
-template<typename T>
-void operator >> (const YAML::Node& node, T& i)
-{
-  i = node.as<T>();
+#include <octomap/OcTreeBase.h>
+#include <octomap/octomap.h>
+
+#include <octomap_msgs/Octomap.h>
+#include <octomap_msgs/conversions.h>
+
+void printUsage(const char *programName) {
+  ROS_ERROR("Usage: %s [-f | -b] filename", programName);
+  ROS_ERROR("    -f forces to send a full map");
+  ROS_ERROR("    -b forces to send a binary map");
+  ROS_ERROR("    -l latch published message");
 }
-#endif
 
-class MapServer
-{
-  public:
-    /** Trivial constructor */
-    MapServer(const std::string& fname, double res)
-    {
-      std::string mapfname = "";
-      double origin[3];
-      int negate;
-      double occ_th, free_th;
-      MapMode mode = TRINARY;
-      std::string frame_id;
-      ros::NodeHandle private_nh("~");
-      private_nh.param("frame_id", frame_id, std::string("map"));
-      deprecated = (res != 0);
-      if (!deprecated) {
-        //mapfname = fname + ".pgm";
-        //std::ifstream fin((fname + ".yaml").c_str());
-        std::ifstream fin(fname.c_str());
-        if (fin.fail()) {
-          ROS_ERROR("Map_server could not open %s.", fname.c_str());
-          exit(-1);
-        }
-#ifdef HAVE_YAMLCPP_GT_0_5_0
-        // The document loading process changed in yaml-cpp 0.5.
-        YAML::Node doc = YAML::Load(fin);
-#else
-        YAML::Parser parser(fin);
-        YAML::Node doc;
-        parser.GetNextDocument(doc);
-#endif
-        try {
-          doc["resolution"] >> res;
-        } catch (YAML::InvalidScalar &) {
-          ROS_ERROR("The map does not contain a resolution tag or it is invalid.");
-          exit(-1);
-        }
-        try {
-          doc["negate"] >> negate;
-        } catch (YAML::InvalidScalar &) {
-          ROS_ERROR("The map does not contain a negate tag or it is invalid.");
-          exit(-1);
-        }
-        try {
-          doc["occupied_thresh"] >> occ_th;
-        } catch (YAML::InvalidScalar &) {
-          ROS_ERROR("The map does not contain an occupied_thresh tag or it is invalid.");
-          exit(-1);
-        }
-        try {
-          doc["free_thresh"] >> free_th;
-        } catch (YAML::InvalidScalar &) {
-          ROS_ERROR("The map does not contain a free_thresh tag or it is invalid.");
-          exit(-1);
-        }
-        try {
-          std::string modeS = "";
-          doc["mode"] >> modeS;
+enum class SendType { BINARY, FULL, AUTO };
 
-          if(modeS=="trinary")
-            mode = TRINARY;
-          else if(modeS=="scale")
-            mode = SCALE;
-          else if(modeS=="raw")
-            mode = RAW;
-          else{
-            ROS_ERROR("Invalid mode tag \"%s\".", modeS.c_str());
-            exit(-1);
-          }
-        } catch (YAML::Exception &) {
-          ROS_DEBUG("The map does not contain a mode tag or it is invalid... assuming Trinary");
-          mode = TRINARY;
-        }
-        try {
-          doc["origin"][0] >> origin[0];
-          doc["origin"][1] >> origin[1];
-          doc["origin"][2] >> origin[2];
-        } catch (YAML::InvalidScalar &) {
-          ROS_ERROR("The map does not contain an origin tag or it is invalid.");
-          exit(-1);
-        }
-        try {
-          doc["image"] >> mapfname;
-          // TODO: make this path-handling more robust
-          if(mapfname.size() == 0)
-          {
-            ROS_ERROR("The image tag cannot be an empty string.");
-            exit(-1);
-          }
+enum class TreeType { BINARY_TREE, OC_TREE, INVALID_TREE };
 
-          boost::filesystem::path mapfpath(mapfname);
-          if (!mapfpath.is_absolute())
-          {
-            boost::filesystem::path dir(fname);
-            dir = dir.parent_path();
-            mapfpath = dir / mapfpath;
-            mapfname = mapfpath.string();
-          }
-        } catch (YAML::InvalidScalar &) {
-          ROS_ERROR("The map does not contain an image tag or it is invalid.");
-          exit(-1);
-        }
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "hello_world_map_server",
+            ros::init_options::AnonymousName);
+
+  // Quick and dirty argument parser
+  if (argc < 2) {
+    printUsage(argv[0]);
+    return -1;
+  }
+
+  std::string filename;
+  bool isLatch = false;
+  auto sendType = SendType::AUTO;
+  auto treeType = TreeType::INVALID_TREE;
+  {
+    bool done = false;
+    size_t index = 1;
+    while (!done) {
+      const auto arg = std::string(argv[index]);
+      if (arg == "-l") {
+        isLatch = true;
+      } else if (arg == "-b") {
+        sendType = SendType::BINARY;
+      } else if (arg == "-f") {
+        sendType = SendType::FULL;
       } else {
-        private_nh.param("negate", negate, 0);
-        private_nh.param("occupied_thresh", occ_th, 0.65);
-        private_nh.param("free_thresh", free_th, 0.196);
-        mapfname = fname;
-        origin[0] = origin[1] = origin[2] = 0.0;
+        filename = argv[index];
+        done = true;
       }
 
-      ROS_INFO("Loading map from image \"%s\"", mapfname.c_str());
-      try
-      {
-          map_server::loadMapFromFile(&map_resp_,mapfname.c_str(),res,negate,occ_th,free_th, origin, mode);
-      }
-      catch (std::runtime_error e)
-      {
-          ROS_ERROR("%s", e.what());
-          exit(-1);
-      }
-      // To make sure get a consistent time in simulation
-      ros::Time::waitForValid();
-      map_resp_.map.info.map_load_time = ros::Time::now();
-      map_resp_.map.header.frame_id = frame_id;
-      map_resp_.map.header.stamp = ros::Time::now();
-      ROS_INFO("Read a %d X %d map @ %.3lf m/cell",
-               map_resp_.map.info.width,
-               map_resp_.map.info.height,
-               map_resp_.map.info.resolution);
-      meta_data_message_ = map_resp_.map.info;
+      ++index;
 
-      service = n.advertiseService("static_map", &MapServer::mapCallback, this);
-      //pub = n.advertise<nav_msgs::MapMetaData>("map_metadata", 1,
+      done |= index >= argc;
+    };
 
-      // Latched publisher for metadata
-      metadata_pub= n.advertise<nav_msgs::MapMetaData>("map_metadata", 1, true);
-      metadata_pub.publish( meta_data_message_ );
-
-      // Latched publisher for data
-      map_pub = n.advertise<nav_msgs::OccupancyGrid>("map", 1, true);
-      map_pub.publish( map_resp_.map );
+    if (filename.empty()) {
+      ROS_ERROR("No input filename was given");
+      printUsage(argv[0]);
+      return -1;
     }
 
-  private:
-    ros::NodeHandle n;
-    ros::Publisher map_pub;
-    ros::Publisher metadata_pub;
-    ros::ServiceServer service;
-    bool deprecated;
+    // pre-determine file type
+    const auto extension = filename.substr(filename.find_last_of('.') + 1);
 
-    /** Callback invoked when someone requests our service */
-    bool mapCallback(nav_msgs::GetMap::Request  &req,
-                     nav_msgs::GetMap::Response &res )
-    {
-      // request is empty; we ignore it
-
-      // = operator is overloaded to make deep copy (tricky!)
-      res = map_resp_;
-      ROS_INFO("Sending map");
-
-      return true;
+    if (extension == "bt") {
+      treeType = TreeType::BINARY_TREE;
+    } else if (extension == "ot") {
+      treeType = TreeType::OC_TREE;
     }
 
-    /** The map data is cached here, to be sent out to service callers
-     */
-    nav_msgs::MapMetaData meta_data_message_;
-    nav_msgs::GetMap::Response map_resp_;
-
-    /*
-    void metadataSubscriptionCallback(const ros::SingleSubscriberPublisher& pub)
-    {
-      pub.publish( meta_data_message_ );
+    if (treeType == TreeType::INVALID_TREE) {
+      ROS_ERROR("Invalid file type. Extension should be either .ot or .bt");
+      printUsage(argv[0]);
+      return -1;
     }
-    */
-
-};
-
-int main(int argc, char **argv)
-{
-  ros::init(argc, argv, "map_server", ros::init_options::AnonymousName);
-  if(argc != 3 && argc != 2)
-  {
-    ROS_ERROR("%s", USAGE);
-    exit(-1);
   }
-  if (argc != 2) {
-    ROS_WARN("Using deprecated map server interface. Please switch to new interface.");
-  }
-  std::string fname(argv[1]);
-  double res = (argc == 2) ? 0.0 : atof(argv[2]);
 
-  try
-  {
-    MapServer ms(fname, res);
+  // Setup publisher, read file and send tree over
+  try {
+    ros::NodeHandle nodeHandle; // w/o namespace
+    auto octomapPublisher =
+        nodeHandle.advertise<octomap_msgs::Octomap>("octomap", 1, isLatch);
+
+    //
+    octomap_msgs::Octomap octomapMsg;
+
+    if (treeType == TreeType::OC_TREE) {
+      // 1. File type was an octree
+
+      const auto ocTreeBase = std::shared_ptr<octomap::AbstractOcTree>(
+          octomap::AbstractOcTree::read(filename));
+
+      if (sendType == SendType::BINARY) {
+        // Forced to send as binary
+        const auto ocTree =
+            std::dynamic_pointer_cast<octomap::OcTree>(ocTreeBase);
+        if (ocTree) {
+          ROS_INFO("Send occupancy tree as binary");
+          octomap_msgs::binaryMapToMsg(*ocTree, octomapMsg);
+        } else {
+          throw std::runtime_error("Cannot publish tree");
+        }
+
+      } else {
+        // Send as normally would otherwise
+        ROS_INFO("Send occupancy tree as full map");
+        octomap_msgs::fullMapToMsg(*ocTreeBase, octomapMsg);
+      }
+
+    } else {
+      // 2. File type was a binary tree
+
+      auto ocTree = octomap::OcTree(filename);
+
+      if (sendType == SendType::FULL) {
+        // Forced to send as full map
+        ROS_INFO("Send binary tree as full map");
+        octomap_msgs::fullMapToMsg(ocTree, octomapMsg);
+      } else {
+        // send as binary
+        ROS_INFO("Send binary tree as binary");
+        octomap_msgs::binaryMapToMsg(ocTree, octomapMsg);
+      }
+    }
+
+
+    octomapMsg.header.frame_id = "base_link";
+    octomapPublisher.publish(octomapMsg);
+
+    ROS_INFO("OK. Message length = %d bytes", octomapMsg.data.size());
+
     ros::spin();
-  }
-  catch(std::runtime_error& e)
-  {
+  } catch (std::runtime_error &e) {
     ROS_ERROR("map_server exception: %s", e.what());
     return -1;
   }
 
   return 0;
 }
-
